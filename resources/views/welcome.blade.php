5e10f0c2d468bfb75a403dcc033fcefee9b7d2e2
anthonymars <anthony.marstiller@gmail.com>
2017-10-26 09:07:10 -0400

    another push

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    New Dev Title
                 </div>
 
                 <div class="links">
